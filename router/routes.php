ff074d743d9d091e5d94974b8d88ca30f8fc946f
MartinKrsek <martin.krsek1@gmail.com>
2021-05-05 16:33:50 +0200

    fix: basic project structure

---
+++ router/routes.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once("../controllers/QuestionMultipleController.php");
+require_once("../controllers/QuestionConnectController.php");
+
+require_once("../controllers/AnswerMultipleController.php");
+require_once("../controllers/AnswerConnectController.php");
+
+require_once("../controllers/ExamController.php");
+
+use Pecee\SimpleRouter\SimpleRouter as Router;
+
+Router::post("Final/router/question/multiple", function () {
+
+    $questionMultipleController = new QuestionMultipleController();
+    $input = input()->all();
+
+    $questionMultipleController->insertQuestionAndAnswers($input);
+});
+
+Router::post("Final/router/question/connect", function () {
+
+    $questionConnectController = new QuestionConnectController();
+    $input = input()->all();
+
+    $questionConnectController->insertQuestionAndAnswers($input);
+});
+
+Router::post("Final/router/exam/insertAnswersMultiple", function () {
+
+    $answerMultipleController = new AnswerMultipleController();
+    $input = input()->all();
+
+    $answerMultipleController->insertAnswers($input["questionId"], $input["studentExamId"], $input["answers"]);
+});
+
+Router::post("Final/router/exam/insertAnswersConnect", function () {
+
+    $answerConnectController = new AnswerConnectController();
+    $input = input()->all();
+
+    $answerConnectController->insertAnswers($input["studentExamId"], $input["questionId"], $input["pairs"]);
+});
+
+Router::get("Final/router/exam/{id}", "ExamController@getExam");
+
+    // set session based on ip and gps permission
+    //Router::post("Forward_the_Foundation/question/multiple", "QuestionMultipleController@insertQuestionAndAnswers");
