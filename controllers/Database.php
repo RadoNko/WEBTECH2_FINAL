76e97564fe50fdab6682d2dde19dc7c3d0024428
MartinKrsek <martin.krsek1@gmail.com>
2021-05-05 18:21:21 +0200

    fix: database name pull from config file

--- controllers/Database.php
+++ controllers/Database.php
@@ -4,30 +4,30 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once($_SERVER['DOCUMENT_ROOT']."/config.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/config.php");
 
-class Database{
+class Database
+{
 
   private $conn;
 
-  public function getConnection(){
-    
+  public function getConnection()
+  {
+
     $this->conn = null;
-                            // IGNORE THESE ERRORS, CODE WORKS ON SERVER, INTELEPHENSE IS CONFUSED
-    try{
-      $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=semestralka", DB_USER, DB_PASS);
-      
+    // IGNORE THESE ERRORS, CODE WORKS ON SERVER, INTELEPHENSE IS CONFUSED
+    try {
+      $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+
       // set the PDO error mode to exception
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
+
       //echo "Connected successfully";
 
-    }
-    catch(PDOException $e){
+    } catch (PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
     }
 
     return $this->conn;
   }
 }
-?>
