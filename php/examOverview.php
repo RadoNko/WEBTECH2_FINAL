03776e437395c71923e947bbbb862dc6217b7ea1
MartinKrsek <martin.krsek1@gmail.com>
2021-05-06 17:42:25 +0200

    fix: anchor into button for future modal functionality

--- php/examOverview.php
+++ php/examOverview.php
@@ -12,11 +12,11 @@
 </head>
 
 <body class="container">
-    <a href="https://www.youtube.com/watch?v=K8BnhF2WgHU&ab_channel=MarekVnen%C4%8D%C3%A1k" id="addNewExamAnchor">
+    <button id="addNewExamButton">
         <svg width="40px" height="40px" aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
         </svg>
-    </a>
+    </button>
     <div id=listBody></div>
     <div class="modal" tabindex="-1" id="toggleExamModal">
         <div class="modal-dialog">
@@ -33,6 +33,25 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="button" class="btn btn-primary" id="toggleExamButton">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal" tabindex="-1" id="createExamModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="createExamTitle"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="toggleExamApprovalBody">
+                    <p id="createExamBody">Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="createExamButton">Save changes</button>
                 </div>
             </div>
         </div>
