34bc47b6076d0f9e85948f74bc95159bc4b4c408
Fullajtar <fullajtar.dominik@gmail.com>
2021-05-06 22:14:55 +0200

    init math controllers

--- controllers/AnswerMathController.php
+++ controllers/AnswerMathController.php
@@ -1,1 +1,50 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once "Database.php";
+
+class AnswerMathController{
+    private PDO $conn;
+
+    public function __construct(){
+        $this->conn = (new Database())->getConnection();
+    }
+
+    private function insertAnswer($question_fk, $answer, $student_exam_fk){
+
+        try{
+            $sql = "INSERT INTO AnswerTypeMath(question_type_fk, answer, student_exam_fk) VALUES(?, ?, ?)";
+            $stm = $this->conn->prepare($sql);
+            $stm->execute([$question_fk,$answer,$student_exam_fk]);
+
+            //return $this->conn->lastInsertId();
+        }
+        catch(PDOException $e){
+            echo "<div class='alert alert-danger' role='alert'>
+                        Sorry, there was an error. " . $e->getMessage()."
+                    </div>";
+        }
+
+    }
+
+    private function setScore($points, $questionId){ //score set manually by teacher
+
+        try{
+            $stmt = $this->conn->prepare("UPDATE AnswerTypeMath SET points =:points WHERE id =:questionId");
+            $stmt->bindParam(":points", $points); //TODO add PDO:: for float
+            $stmt->bindParam(":questionId", $questionId, PDO::PARAM_INT);
+            $stmt->execute();
+
+            //return $this->conn->lastInsertId();
+        }
+        catch(PDOException $e){
+            echo "<div class='alert alert-danger' role='alert'>
+                        Sorry, there was an error. " . $e->getMessage()."
+                    </div>";
+        }
+
+    }
+
+}
--- controllers/QuestionMathController.php
+++ controllers/QuestionMathController.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 require_once "Database.php";
 
-class QuestionText{
+class QuestionMathController{
     public $connection;
 
     function __construct() {
@@ -33,28 +33,11 @@ class QuestionText{
 
     }
 
-//    private function insertAnswer($question_fk,$answer,$student_exam_fk, $points){
-//
-//        try{
-//            $sql = "INSERT INTO AnswerTypeText(question_type_fk, answer, student_exam_fk, points) VALUES(?, ?, ?,?)";
-//            $stm = $this->connection->prepare($sql);
-//            $stm->execute([$question_fk,$answer,$student_exam_fk,$points]);
-//
-//            //return $this->conn->lastInsertId();
-//        }
-//        catch(PDOException $e){
-//            echo "<div class='alert alert-danger' role='alert'>
-//                        Sorry, there was an error. " . $e->getMessage()."
-//                    </div>";
-//        }
-//
-//    }
-
     private function insertQuestion($name, $exam, $maxPoints){
 
         try{
 
-            $sql = "INSERT INTO QuestionTypeText(name, exam_fk, max_points) VALUES(?,?,?)";
+            $sql = "INSERT INTO QuestionTypeMath(name, exam_fk, max_points) VALUES(?,?,?)";
 
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $stm = $this->connection->prepare($sql);
@@ -73,12 +56,12 @@ class QuestionText{
 
     public function addQuestion($data){
 
-//        $examId = $this->insertExam(1);
+       $examId = 1;//TODO //$this->insertExam(1);
 
         foreach($data as $key => $value){
 
             if($key === "question"){
-                $questionId = $this->insertQuestion($data["question"], $data["answer"],1);
+                $questionId = $this->insertQuestion($data["question"], $examId, $data["points"]);
             }
         }
         return json_encode($questionId);
