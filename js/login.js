4802fe9171e67f4f84d2ec098cbd7116d2d98392
kandy <matus.kanda.po@gmail.com>
2021-05-13 21:41:06 +0200

    opraveny autosubmit po logine studenta

--- js/login.js
+++ js/login.js
@@ -127,15 +127,15 @@ function verifyTeacherLogin(){
 }
 
 
-function logOut(){
-    const origin = $(location).attr("origin");
-    $.ajax({
-        method: "POST",
-        url: origin+"/Final/router/logins/destroySession",
-        data: {},
-        success: function (data) {
-            location.href = "/Final/index.php";
-        }
-    });
-}
+// function logOut(){
+//     const origin = $(location).attr("origin");
+//     $.ajax({
+//         method: "POST",
+//         url: origin+"/Final/router/logins/destroySession",
+//         data: {},
+//         success: function (data) {
+//             location.href = "/Final/index.php";
+//         }
+//     });
+// }
 
